3bf5396cae968b8a616b4b0effce8a9781c0a063
kkylych <127922323+kkylych@users.noreply.github.com>
2023-12-01 15:30:30 +0600

    Delete Calculator directory

--- Calculator/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.calculator {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens com.example.calculator to javafx.fxml;
-    exports com.example.calculator;
-}
